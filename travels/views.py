f8f25703c808e8a68f221e5606749734adfe4550
brandonmizutani <bran2miz@gmail.com>
2022-03-19 16:13:44 -0700

    finished lab for permissions and postgres

--- travels/views.py
+++ travels/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializer import TravelSerializer
 from .models import Travel
+from .permissions import IsOwnerOrReadOnly
 
 class TravelList(generics.ListCreateAPIView):
+  permission_classes=(IsOwnerOrReadOnly,)
   queryset = Travel.objects.all()
   serializer_class = TravelSerializer
 
 class TravelDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes=(IsOwnerOrReadOnly,)
   queryset = Travel.objects.all()
   serializer_class = TravelSerializer
